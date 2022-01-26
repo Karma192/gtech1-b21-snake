57fdf980e0e425abf6b66721b3f95a2037d2df13
hazmat <ebertrand@gaming.tech>
2022-01-26 15:02:21 +0100

    carré carré

--- ebertrand/exo3.cpp
+++ ebertrand/exo3.cpp
@@ -1,44 +1,59 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
-void keyboard(){
-const Uint8 *state = SDL_GetKeyboardState(NULL);
-  if (state[SDL_SCANCODE_UP]) {
+using namespace std;
+const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+
+int main(int argc, char *argv[]){
+
+  SDL_Window* fenetre;  // Déclaration de la fenêtre
+
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)  // initialisation de la SDL
     {
-       printf("yes ça marche");
-    }
-  }
-  else{
-      printf("oups");
-  }
-}
-
-int main(int argc, char *argv[])
-{
-    SDL_Window* fenetre;  // Déclaration de la fenêtre
-
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)  // initialisation de la SDL
-    {
-       printf("Erreur d'initialisation de la SDL : %s", SDL_GetError());
-       return EXIT_FAILURE;
+      printf("Erreur d'initialisation de la SDL : %s", SDL_GetError());
+      return EXIT_FAILURE;
     }
 
-    fenetre = SDL_CreateWindow("Une fenetre SDL",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);  // Création de la fenêtre
+  fenetre = SDL_CreateWindow("Une fenetre SDL",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 600, 0);  // Création de la fenêtre
 
-    if (fenetre == NULL)  //gestion des erreurs
+  if (fenetre == NULL)  //gestion des erreurs
     {
         printf("Erreur lors de la creation d'une fenetre : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    keyboard();
+  SDL_Renderer *renderer;
+  renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    SDL_Delay(10000);  //pause de 10 secondes
+  if(renderer == NULL)//gestion des erreurs
+  {
+    printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
+    return EXIT_FAILURE;
+  }
 
-    SDL_DestroyWindow(fenetre);
-    SDL_Quit();  //on quitte la SDL
-    return 0;
+  SDL_Rect rect;
+  rect.w = 200;
+  rect.h = 200;
+  rect.x = 350;
+  rect.y = 350;
+
+  SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+  SDL_RenderDrawRect(renderer, &rect);
+  SDL_RenderFillRect(renderer, &rect); 
+  SDL_RenderPresent(renderer);
+
+
+  if (keystate[SDL_SCANCODE_UP]){
+      cout << "UP" << endl;
+    }
+    
+  if (keystate[SDL_SCANCODE_DOWN]){
+      cout << "DOWN" << endl;
+  }
+
+  SDL_Delay(4000);
+
+  return 0;
 }
