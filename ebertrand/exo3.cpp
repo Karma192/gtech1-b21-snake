ec84677b8b8966a94d4f84d178c720935d5ced1d
hazmat <ebertrand@gaming.tech>
2022-01-31 09:14:39 +0100

    nothing

--- ebertrand/exo3.cpp
+++ ebertrand/exo3.cpp
@@ -8,6 +8,8 @@ int posity = 285;
 using namespace std;
 SDL_Renderer *renderer; //Déclaration renderer
 SDL_Window* fenetre;  // Déclaration de la fenêtre
+Uint32 frame_rate = 17;//FrameRate
+Uint32 frame_time_start = SDL_GetTicks(); 
 
 int map(void){
   if(SDL_Init(SDL_INIT_VIDEO) < 0)  // initialisation de la SDL
@@ -45,9 +47,9 @@ int keys(void){
   SDL_Event event;
   SDL_PollEvent(&event);
 
-
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
+
   SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
   SDL_RenderDrawRect(renderer, &rect);
   SDL_RenderFillRect(renderer, &rect); 
@@ -55,7 +57,7 @@ int keys(void){
 
   const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
-  if (keystate[SDL_SCANCODE_UP]) {
+  if (keystate[SDL_SCANCODE_UP] && i!=-1) {
     while (keystate[SDL_SCANCODE_LEFT]==0 || keystate[SDL_SCANCODE_RIGHT]==0){
       SDL_Delay(50);
       cout << "UP" << endl;
@@ -64,7 +66,7 @@ int keys(void){
       i=1;
     }
   }
-    
+
   if (keystate[SDL_SCANCODE_DOWN] && i!=1) {
     while (keystate[SDL_SCANCODE_LEFT]==0 || keystate[SDL_SCANCODE_RIGHT]==0){
       SDL_Delay(50);
@@ -72,28 +74,31 @@ int keys(void){
       posity+=10;
       keys();
       printf("%d\n", i);
+      i=-1;
     }
   }
 
-  if (keystate[SDL_SCANCODE_RIGHT]) {
+  if (keystate[SDL_SCANCODE_RIGHT] && i!=-2) {
     while (keystate[SDL_SCANCODE_DOWN]==0 || keystate[SDL_SCANCODE_UP]==0){
       SDL_Delay(50);     
       cout << "RIGHT" << endl;
       positx+=10;
       keys();
+      i=2;
     }
   }
 
-  if (keystate[SDL_SCANCODE_LEFT]) { 
+  if (keystate[SDL_SCANCODE_LEFT] && i!=2) { 
     while (keystate[SDL_SCANCODE_DOWN]==0 || keystate[SDL_SCANCODE_UP]==0 || keystate[SDL_SCANCODE_RIGHT]==1){
       SDL_Delay(50);
       cout << "LEFT" << endl;
       positx-=10;
       keys();
+      i=-2;
     }
   }
 
-  printf("%d\n", i);
+  printf("%d\n", *i);
   return 0;
 
 }
