d2ffe2f82cd24aefaf99e661c8fb80ad2efad62f
hazmat <ebertrand@gaming.tech>
2022-02-07 11:25:42 +0100

    copied code from Karma

---
+++ ebertrand/good from Karma/app.cpp
@@ -0,0 +1,62 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+#include "apple.hpp"
+#include "snake.hpp"
+//#include "score.hpp"
+#include "MainSDLWindow.hpp"
+
+#define WIDTHGAME 540
+#define HEIGHTWINDOW 600
+#define SIZE 30
+
+Uint32 frame_rate = 80;
+
+int main(void)
+{
+    Snake snakeH;
+    Apple A;
+    MainSDLWindow sdlwin;
+    if (sdlwin.Init("Snake", WIDTHGAME, HEIGHTWINDOW) == EXIT_FAILURE)
+    {
+        exit(EXIT_FAILURE);
+    }
+    /*Score scwin;
+    if (scwin.Init("Score Snake", 100, 50) == EXIT_FAILURE)
+    {
+        exit(EXIT_FAILURE);
+    }*/
+    int exit = 0;
+    int gameOver = 0;
+    int score = 0;
+    int apple = 0;
+    while (exit == 0 && gameOver == 0)
+    {
+        Uint32 frame_time_start = SDL_GetTicks();
+        sdlwin.map();
+        snakeH.drawHead(sdlwin.GetRenderer());
+        snakeH.keys();
+        A.DrawApple(sdlwin.GetRenderer());
+        snakeH.Move(snakeH.keys());
+        SDL_RenderPresent(sdlwin.GetRenderer());
+        score = score + snakeH.colApple(A.GetPosx(), A.GetPosy());
+        A.colSnake(snakeH.colApple(A.GetPosx(), A.GetPosy()));
+        gameOver = snakeH.colBoard();
+        exit = sdlwin.redCross();
+        Uint32 frame_time_interval = SDL_GetTicks() - frame_time_start;
+        if (frame_time_interval < frame_rate)
+        {
+            SDL_Delay(frame_rate - frame_time_interval);
+        }
+    }
+
+    if (gameOver != 0)
+    {
+        cout << "Game Over..." << endl;
+        cout << "Your score is " << endl;
+        cout << score << endl;
+    }
+}
